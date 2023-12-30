7cfd97d54f818862d60b8123435452d96dae4f2e
navinxx <rajbharnavin6@gmail.com>
2023-12-30 08:39:32 +0530

    f1

--- instagram/settings.py
+++ instagram/settings.py
@@ -14,18 +14,20 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+import os
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-k12jom047avti-w(p%#tpl$du($@$c%hy*5*-0_kfvv4_h$m1j'
+SECRET_KEY =os.environ.get("SECRET_KEY")
+
+#'django-insecure-k12jom047avti-w(p%#tpl$du($@$c%hy*5*-0_kfvv4_h$m1j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG","false").lower() == "true"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 
 
 # Application definition
